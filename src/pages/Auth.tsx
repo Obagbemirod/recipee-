cae4d0dde166ce1609b68e0fa373ed5c1ec81cb9
TEMITOPE OBAGBEMIRO <93609537+Obagbemirod@users.noreply.github.com>
2024-12-02 10:32:04 +0100

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -75,18 +75,9 @@ const Auth = () => {
           </Button>
         </div>
 
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-        </div>
+        
 
-        <SocialAuth isLogin={true} />
+      
       </motion.div>
     </div>
   );
