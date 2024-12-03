e9db9d7948b27a562cd0f866126bfb24a8f371e3
TEMITOPE OBAGBEMIRO <93609537+Obagbemirod@users.noreply.github.com>
2024-12-03 16:32:41 +0100

    Update Forgot.tsx

--- src/pages/Forgot.tsx
+++ src/pages/Forgot.tsx
@@ -25,9 +25,13 @@ const Forgot = () => {
             Enter your New Password and follow the instructions to reset your password...
           </p>
           <div class="mb-6">
-    <label for="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
-    <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-</div>
+             <label for="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+             <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          </div>
+          <div class="mb-6">
+           <label for="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm New Password</label>
+            <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          </div>
 
                 
             <button type="submit" className="w-full">
