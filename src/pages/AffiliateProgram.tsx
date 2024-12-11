8104582874f68b86da855064f518c6d4b29f1576
TEMITOPE OBAGBEMIRO <93609537+Obagbemirod@users.noreply.github.com>
2024-12-11 19:30:51 +0100

    Update AffiliateProgram.tsx

--- src/pages/AffiliateProgram.tsx
+++ src/pages/AffiliateProgram.tsx
@@ -114,12 +114,12 @@ const AffiliateProgram = () => {
               <Card>
                 <CardContent className="p-6">
                   <h2 className="text-2xl font-semibold mb-4">Getting Started</h2>
-                  <p>To join the Recipee Partner Program, we've partnered with *Squaredance*, a leading platform for influencer marketing. Here's how it works:</p>
+                  <p>To join the Recipee Partner Program, we've partnered with Squaredance, a leading platform for influencer marketing. Here's how it works:</p>
                   <ol className="space-y-4 list-decimal list-inside text-muted-foreground">
-                    <li>Sign Up on Squaredance: We've partnered with Squaredance to make joining our affiliate program seamless. Click the link below to create your free Squaredance account.</li>
-                    <li>Apply to the Recipee Partner Program: Once you've signed up, you can easily apply to the Recipee Partner Program within your Squaredance dashboard.</li>
-                    <li>Get Your Unique Link: After your application is approved, you'll receive your unique affiliate link.</li>
-                    <li>Start Sharing & Earning: Promote Recipee to your audience and watch your commissions grow. Get paid weekly every Friday!</li>                 
+                    <li>Signing Up: To make joining our affiliate program seamless, we have chosen the *Zoho Thrive* as our Affiliate Management Platform. Click the link below to create your free account.</li>
+                    <li>Apply to the Recipee Partner Program: Once you've signed up and your application is approved, you will be able to easily access your dashboard</li>
+                    <li>Get Your Unique Link: After your application is approved, you'll receive your unique affiliate link. You can also create more custom links as needed</li>
+                    <li>Start Sharing & Earning: Promote Recipee through your affiliate link(s) to your audience and watch your commissions grow</li>                 
                   </ol>
                   <p>Click Below To Get Started!</p>
                 </CardContent>
